5f2897d2878f45a3c019618c526b9b85002739bd
jolle <jolle@users.noreply.github.com>
2023-12-08 01:29:57 +0200

    Add CORS to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { userCountRoute } from "./routes/userCount";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/user-count", userCountRoute);
 
